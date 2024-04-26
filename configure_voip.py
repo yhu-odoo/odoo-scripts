8bb71aa07b60919ad897b598937fdedd73ce612a
yhu-odoo <yhu@odoo.com>
2024-04-26 10:42:58 +0200

    [IMP] support version >= 17.2

--- configure_voip.py
+++ configure_voip.py
@@ -1,5 +1,9 @@
 import psycopg2
+import re
 import sys
+ODOO_PATH = ''
+sys.path.append(ODOO_PATH)
+import odoo
 
 
 SERVER_CONFIGURATION = {
@@ -17,26 +21,58 @@ def configure_voip(db):
         user="odoo")
     cur = conn.cursor()
 
-    for key, value in SERVER_CONFIGURATION.items():
-        cur.execute(f'''
-        SELECT count(*) FROM ir_config_parameter WHERE KEY = '{key}';
-        ''')
-        if cur.fetchone()[0] > 0:
+    version = re.match(r"[\d\.]+", odoo.release.version).group(0)
+    if version < "17.2":
+        for key, value in SERVER_CONFIGURATION.items():
             cur.execute(f'''
-            UPDATE ir_config_parameter
-            SET value = '{value}'
-            WHERE KEY = '{key}';
+            SELECT count(*) FROM ir_config_parameter WHERE KEY = '{key}';
+            ''')
+            if cur.fetchone()[0] > 0:
+                cur.execute(f'''
+                UPDATE ir_config_parameter
+                SET value = '{value}'
+                WHERE KEY = '{key}';
+                ''')
+            else:
+                cur.execute(f'''
+                INSERT INTO ir_config_parameter(key, value)
+                VALUES ('{key}', '{value}');
+                ''')
+
+        cur.execute(f'''
+            UPDATE res_users_settings
+            SET voip_username = '{USERNAME}', voip_secret = '{SECRET}'
+            WHERE user_id = 2;
+            ''')
+    else:
+        cur.execute('''
+            SELECT id FROM voip_provider WHERE name = 'Default';
+        ''')
+        row = cur.fetchone()
+        if row:
+            provider_id = row[0]
+            cur.execute(f'''
+                UPDATE voip_provider SET
+                    name = 'Default',
+                    ws_server = '{SERVER_CONFIGURATION["voip.wsServer"]}',
+                    pbx_ip = '{SERVER_CONFIGURATION["voip.pbx_ip"]}',
+                    mode = '{SERVER_CONFIGURATION["voip.mode"]}'
+                WHERE id = {provider_id};
             ''')
         else:
             cur.execute(f'''
-            INSERT INTO ir_config_parameter(key, value)
-            VALUES ('{key}', '{value}');
+                INSERT INTO voip_provider (name, ws_server, pbx_ip, mode)
+                VALUES ('Default', '{SERVER_CONFIGURATION["voip.wsServer"]}', '{SERVER_CONFIGURATION["voip.pbx_ip"]}', '{SERVER_CONFIGURATION["voip.mode"]}');
             ''')
+            cur.execute('''
+                SELECT id FROM voip_provider WHERE name = 'Default';
+            ''')
+            provider_id = cur.fetchone()[0]
 
-    cur.execute(f'''
-        UPDATE res_users_settings
-        SET voip_username = '{USERNAME}', voip_secret = '{SECRET}'
-        WHERE user_id = 2;
+        cur.execute(f'''
+            UPDATE res_users_settings
+            SET voip_provider_id = '{provider_id}', voip_username = '{USERNAME}', voip_secret = '{SECRET}'
+            WHERE user_id = 2;
         ''')
 
     conn.commit()
